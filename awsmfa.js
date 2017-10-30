bcb49a90da3a83b0db73d5080fe68ff76da583ec
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2017-10-30 19:04:30 -0400

    Basic argument parsing and profile selection

--- awsmfa.js
+++ awsmfa.js
@@ -1,14 +1,26 @@
 var fs=require('fs');
 var path=require('path');
 var ini=require('ini');
+var parseArgs=require('minimist');
 
+var argv=parseArgs(process.argv);
 filePath=path.join(process.env["HOME"],".aws/credentials");
 fs.readFile(filePath, "utf-8", (err, data)=>{
   if (err) {
     console.log("Error reading " + filePath);
+    process.exit(1);
   } else {
     var config = ini.parse(data);
+    if (argv['profile']) {
+      awsProfile = argv['profile'];
+    } else if (process.env['AWS_PROFILE']) {
+      awsProfile = process.env['AWS_PROFILE'];
+    } else {
+      console.log("Specify profile name to switch to using --profile or $AWS_PROFILE");
+      process.exit(1);
+    }
+
+    console.log("Switching to profile: " + awsProfile);
     console.log(config);
-    
   }
 })
